1d80cecc925c5111408789ec3b2c3fefcf1d9622
Matheu <johnatan.ruales@campusucc.edu.co>
2026-04-12 22:20:27 -0500

    Add header base config and styles

---
+++ src/components/header/config.ts
@@ -0,0 +1,38 @@
+import { Link } from "@/types";
+
+const links: Link[] = [
+  {
+    title: 'Inicio',
+    href: '/',
+  },
+  {
+    title: 'Acerca',
+    href: '/#about',
+  },
+  {
+    title: 'Experiencia',
+    href: '/#experience',
+  },
+  {
+    title: 'Habilidades',
+    href: '/#skills',
+  },
+  {
+    title: 'Proyectos',
+    href: '/#projects',
+  },
+  {
+    title: 'Servicios',
+    href: '/#services',
+  },
+  {
+    title: 'Testimonios',
+    href: '/#testimonials',
+  },
+  {
+    title: 'Contacto',
+    href: '/#contact',
+  }
+];
+
+export { links };
