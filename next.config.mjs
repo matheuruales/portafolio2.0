7b7f8aabbd8d63dd03b4406f4e45ca521aa5833b
Matheu <johnatan.ruales@campusucc.edu.co>
2026-04-12 22:17:50 -0500

    Add Next.js configuration

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compress: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 60 * 60 * 24 * 7, // 7 días
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+};
+
+export default nextConfig;
