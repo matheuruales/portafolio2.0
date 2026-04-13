9f06f5ae9a8cb57ad38ef5e66dba59bf9e3dc3b1
Matheu <johnatan.ruales@campusucc.edu.co>
2026-04-12 22:21:08 -0500

    Add contact form component

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,149 @@
+"use client";
+import { Check, ChevronRight, Loader2 } from "lucide-react";
+import React from "react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/ace-input";
+import { Textarea } from "./ui/ace-textarea";
+import { cn } from "@/lib/utils";
+import { useToast } from "./ui/use-toast";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+import { useLang } from "@/contexts/language";
+
+const ContactForm = () => {
+  const [fullName, setFullName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+
+  const { t } = useLang();
+  const { toast } = useToast();
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch("/api/send", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          fullName,
+          email,
+          message,
+        }),
+      });
+      const data = await res.json();
+      if (data.error) throw new Error(data.error);
+      toast({
+        title: t.form.success_title,
+        description: t.form.success_desc,
+        variant: "default",
+        className: cn("top-0 mx-auto flex fixed md:top-4 md:right-4"),
+      });
+      setLoading(false);
+      setFullName("");
+      setEmail("");
+      setMessage("");
+      const timer = setTimeout(() => {
+        router.push("/");
+        clearTimeout(timer);
+      }, 1000);
+    } catch (err) {
+      toast({
+        title: t.form.error_title,
+        description: t.form.error_desc,
+        className: cn(
+          "top-0 w-full flex justify-center fixed md:max-w-7xl md:top-4 md:right-4"
+        ),
+        variant: "destructive",
+      });
+    }
+    setLoading(false);
+  };
+  return (
+    <form className="w-full mx-auto sm:mt-4" onSubmit={handleSubmit}>
+      <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+        <LabelInputContainer>
+          <Label htmlFor="fullname">{t.form.name_label}</Label>
+          <Input
+            id="fullname"
+            placeholder={t.form.name_placeholder}
+            type="text"
+            required
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+          />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="email">{t.form.email_label}</Label>
+          <Input
+            id="email"
+            placeholder={t.form.email_placeholder}
+            type="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </LabelInputContainer>
+      </div>
+      <div className="grid w-full gap-1.5 mb-4">
+        <Label htmlFor="content">{t.form.message_label}</Label>
+        <Textarea
+          placeholder={t.form.message_placeholder}
+          id="content"
+          required
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <p className="text-sm text-muted-foreground">
+          {t.form.privacy}
+        </p>
+      </div>
+      <Button
+        disabled={loading}
+        className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+        type="submit"
+      >
+        {loading ? (
+          <div className="flex items-center justify-center">
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            <p>{t.form.loading}</p>
+          </div>
+        ) : (
+          <div className="flex items-center justify-center">
+            {t.form.submit} <ChevronRight className="w-4 h-4 ml-4" />
+          </div>
+        )}
+        <BottomGradient />
+      </Button>
+    </form>
+  );
+};
+
+export default ContactForm;
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-brand to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent orange-400 to-transparent" />
+    </>
+  );
+};
